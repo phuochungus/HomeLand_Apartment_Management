69866a1b43e95204856b8dce51f7020d83a71add
NMPhap <21522441@gm.uit.edu.vn>
2023-10-24 08:51:12 +0700

    <Dev>: Add Loading filter

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { endpoint } from "@/constraints/endpoints";
 export async function validateToken(token: string | undefined): Promise<boolean> {
-    if(!token)
+    if (!token)
         return false;
     const result = await fetch(endpoint.tokenValidate, {
         method: "get", headers: {
@@ -10,7 +10,32 @@ export async function validateToken(token: string | undefined): Promise<boolean>
             'Content-Type': 'application/json'
         },
     })
-    .then(res => true)
-    .catch(err => { console.log(err); return false })
+        .then(res => true)
+        .catch(err => { console.log(err); return false })
     return result;
+}
+
+export function loadingFiler(document: Document, container: HTMLElement) {
+    var div = document.createElement("div");
+    div.className = "loadingFilter";
+    div.style.backgroundColor = "black";
+    div.style.width = "100%";
+    div.style.height = "100%";
+    div.style.opacity = "0.2";
+    div.style.position = "absolute";
+    div.style.display = "flex";
+    div.style.justifyContent = "center";
+    div.style.alignContent = "center";
+    div.style.flexWrap = "wrap";
+    var spinner = document.createElement("div");
+    spinner.className = "spinner-border";
+    spinner.style.color = "white";
+    div.appendChild(spinner);
+    container.appendChild(div);
+}
+export function removeLoadingFilter(container: HTMLElement) {
+    var temp = container.getElementsByClassName("loadingFilter")
+    if (temp.length == 0)
+        return
+    container.removeChild(temp[0]);
 }
