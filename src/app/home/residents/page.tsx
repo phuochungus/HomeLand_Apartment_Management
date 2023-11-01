f742f6325e0e39a5436c6f01fd4fcd7ce54976be
vocongbinh <binh93phudong@gmail.com>
2023-11-01 19:42:36 +0700

    fix

--- src/app/home/residents/page.tsx
+++ src/app/home/residents/page.tsx
@@ -1,13 +1,227 @@
-"use client"
-import { useTranslation } from "react-i18next";
+"use client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import ButtonComponent from "@/components/buttonComponent/buttonComponent";
 import styles from "../page.module.css";
+import residentStyles from "./resident.module.scss";
+import utilStyles from "@/styles/utils.module.scss";
+import { clsx } from "clsx";
+import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
+import SearchLayout from "@/components/searchLayout/searchLayout";
+import {
+  AddResidentIcon,
+  CloseIcon,
+  EditIcon,
+  SortIcon,
+} from "@/components/icons";
+import { useState, useEffect, ReactNode, createRef } from "react";
+import ModalComponent from "@/components/Modal/Modal";
+import { useRouter } from "next/navigation";
+import { futuna } from "../../../../public/fonts/futura";
+import { format } from "date-fns";
+import { Resident } from "@/models/resident";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { search } from "@/libs/utils";
+
 export default function Residents() {
-  const [t, i18n] = useTranslation();
+  const [showModal, setShowModal] = useState(false);
+  const [residents, setResidents] = useState<Array<Resident>>([]);
+  const [selectedId, setSelectedId] = useState("");
+  const searchRef = createRef<HTMLInputElement>();
+  const listOptions = [
+    {
+      value: 10,
+      // handleActive: () => handleSetActive(10),
+      // className: clsx({ active: maxPageDisplay === 10 }),
+    },
+    {
+      value: 20,
+      // handleActive: () => handleSetActive(20),
+      // className: cx({ active: maxPageDisplay === 20 }),
+    },
+    {
+      value: 50,
+      // handleActive: () => handleSetActive(50),
+      // className: cx({ active: maxPageDisplay === 50 }),
+    },
+    {
+      value: 100,
+      // handleActive: () => handleSetActive(100),
+      // className: cx({ active: maxPageDisplay === 100 }),
+    },
+  ];
+  const deleleHandle = (id: string) => {
+    setSelectedId(id);
+    setShowModal(true);
+  };
+  const retrieveResidents = async () => {
+    try {
+      const res = await axios.get("/api/resident");
+      setResidents(res.data);
+      console.log(res.data);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const { isLoading, isError, data, refetch } = useQuery(
+   "residents",
+    retrieveResidents,
+    
+  );
+  const titleTable = [
+    "ID",
+    "Tên",
+    "Tài khoản",
+    "Căn hộ",
+    "Số điện thoại",
+    "Ngày tạo",
+  ];
+  const handleSearch = async (e: any) => {
+    if (e.key === "Enter") {
+      console.log("hah");
+      try {
+        const res = await axios.get("/api/resident/search", {
+          params: {
+            query: searchRef.current?.value,
+          },
+        });
+        console.log(res.data);
+        setResidents(res.data);
+      } catch (e) {
+        alert(e);
+      }
+    }
+  };
+  const searchIconClick = async () => {
+    console.log("hah");
+    try {
+      const res = await axios.get("/api/resident/search", {
+        params: {
+          query: searchRef.current?.value,
+        },
+      });
+      console.log(res.data);
+      setResidents(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const handleConfirmDelete = async (id: string) => {
+    console.log(id);
+    setShowModal(false);
+    try {
+      await axios.delete(`/api/resident/${id}`);
+      refetch();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <main className={styles.main}>
-      {" "}
-      <div>{t("home.body")}</div>
+    <main className={clsx(styles.main)}>
+      <div className={clsx(residentStyles.wrapper)}>
+        <h1 className={clsx(utilStyles.headingXl)}>Quản lí căn hộ</h1>
+        <div className={clsx(residentStyles.header)}>
+          <h1 className={clsx(utilStyles.headingLg)}>Danh sách cư dân</h1>
+          <ButtonComponent
+            href="/home/residents/addResident?auth=true"
+            preIcon={<AddResidentIcon width={24} height={24} />}
+            className={clsx(residentStyles.addBtn, futuna.className)}
+          >
+            Tạo cư dân
+          </ButtonComponent>
+        </div>
+        <div className="d-flex w-100 mt-3 justify-content-between">
+          <div className={clsx(residentStyles.perPage)}>
+            <span>Show</span>
+            <span>
+              <Form.Select aria-label="Default select example">
+                {listOptions.map(
+                  (option, index): JSX.Element => (
+                    <option key={index} value={index}>
+                      {option.value}
+                    </option>
+                  )
+                )}
+              </Form.Select>
+            </span>
+            <span>Entries</span>
+          </div>
+          <SearchLayout
+            onKeydown={handleSearch}
+            iconClick={searchIconClick}
+            placeHolder="tìm dân cư..."
+            ref={searchRef}
+          />
+        </div>
+        <div className="w-100 mt-5">
+          <Table
+            className={clsx(residentStyles.tableResident, futuna.className)}
+            striped
+            bordered
+            hover
+          >
+            <thead>
+              <tr>
+                {titleTable.map((title: String, index) => (
+                  <th key={index}>
+                    {title} <SortIcon width={12} height={12} />
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {residents.map((resident, index): ReactNode => {
+                const time = new Date(resident.created_at);
+                const createAt = format(time, "yyyy-MM-dd HH:mm:ss");
+
+                return (
+                  <tr key={index}>
+                    <td>{resident.id}</td>
+                    <td>{resident.profile.name}</td>
+                    <td>{resident.payment_info}</td>
+                    <td>{resident.stay_at && resident.stay_at.name}</td>
+                    <td>{resident.profile.phone_number}</td>
+                    <td>{createAt}</td>
+                    <td style={{ width: 20 }}>
+                      <div className="d-flex">
+                        <ButtonComponent
+                          preIcon={<EditIcon width={16} height={16} />}
+                          className={clsx(
+                            residentStyles.cudBtn,
+                            residentStyles.editBtn
+                          )}
+                          href={`/home/residents/updateResident/${resident.id}/?auth=true`}
+                        >
+                          Sửa
+                        </ButtonComponent>
+                        <ButtonComponent
+                          onClick={() => deleleHandle(resident.id)}
+                          preIcon={<CloseIcon width={16} height={16} />}
+                          className={clsx(
+                            residentStyles.cudBtn,
+                            residentStyles.deleteBtn
+                          )}
+                        >
+                          Xóa
+                        </ButtonComponent>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+      <ModalComponent
+        show={showModal}
+        title="Có chắc chắn xóa cư dân này?"
+        handleConfirm={() => handleConfirmDelete(selectedId)}
+        setShow={setShowModal}
+      />
     </main>
   );
 }
