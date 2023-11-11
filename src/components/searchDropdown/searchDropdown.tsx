1db051921fea6acdf592f954cde99d88861065cf
ManhHoDinh <21522327@gm.uit.edu.vn>
2023-11-11 22:21:29 +0700

    change to scrool search dropdown

--- src/components/searchDropdown/searchDropdown.tsx
+++ src/components/searchDropdown/searchDropdown.tsx
@@ -19,7 +19,14 @@ const CustomMenu = React.forwardRef<any, any>(
           onChange={(e) => setValue(e.target.value)}
           value={value}
         />
-        <ul className="list-unstyled">
+        <ul
+          className="list-unstyled"
+          style={{
+            overflowY: "auto",
+            maxHeight: "200px",
+            width: "100%",
+          }}
+        >
           {React.Children.toArray(children).filter(
             (child: any) =>
               !value || child.props.children.toLowerCase().startsWith(value)
