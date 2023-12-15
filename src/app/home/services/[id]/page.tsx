5e878c949ecc9cffac504f3f1f6a3afd9f41cb71
Daiduong1593572468 <daiduong35578@gmail.com>
2023-12-15 13:08:50 +0700

    daiduong

--- src/app/home/services/[id]/page.tsx
+++ src/app/home/services/[id]/page.tsx
@@ -565,7 +565,7 @@ export default function Page({ params }: { params: { id: string } }) {
                     borderRadius: "20px",
                     margin: "20px 0px",
                     paddingTop: "20px",
-                    width: isMobile ? '100%' :is1200 ? '100%' :  isSmailSceen ? '70%' :  `${feedback.comment.length > 10 ? Math.min(feedback.comment.length, commentMaxLength) *30 : 350}px`,
+                    width: isMobile ? '100%' :is1200 ? '90%' :  isSmailSceen ? '70%' :  `${feedback.comment.length > 10 ? Math.min(feedback.comment.length, commentMaxLength) *30 : 350}px`,
                     whiteSpace: 'pre-wrap',
                     overflow: "hidden",
                     textOverflow: "ellipsis",
