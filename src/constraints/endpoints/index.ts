8d8e907117fda1c28f6c4eb167fd50b715603228
vocongbinh <binh93phudong@gmail.com>
2024-01-08 23:30:34 +0700

    fix sidebar + change language

--- src/constraints/endpoints/index.ts
+++ src/constraints/endpoints/index.ts
@@ -1,5 +1,5 @@
-export const baseUrl = "https://homeland-be-eumo.onrender.com";
-// const baseUrl = "http://localhost:5002";
+// export const baseUrl = "https://homeland-be-eumo.onrender.com";
+const baseUrl = "http://localhost:5002";
 // export const baseUrl = "http://localhost:8080";
 export const endpoint = {
   login: baseUrl + "/auth/signin",
